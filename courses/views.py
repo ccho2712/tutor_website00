8c28dfcac2f6cd6d61b7ba16779e7c31297b1871
leonho <ccho2712@gmail.com>
2025-10-22 12:36:04 +0800

    step2.1: course page setting

--- courses/views.py
+++ courses/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Course
-#from tutors.models import Tutor
+from tutors.models import Tutor
 # Create your views here.
 def courses(request):
     courses = Course.objects.all()
+    mvp_tutors = Tutor.objects.all().filter(is_mvp=True)
 
     context = {
         'courses': courses,
-        
+        'mvp_tutors': mvp_tutors,
     }
     return render(request, 'courses/courses.html', context)
 
 def course(request, course_id):
     course = Course.objects.get(id=course_id)
+    mvp_tutors = Tutor.objects.all().filter(is_mvp=True)
     context = {
         'course': course,
+        'mvp_tutors': mvp_tutors,
     }
     return render(request, 'courses/course.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from tutors.models import Tutor
+from courses.models import Course
 
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
 def about(request):
-    return render(request, 'pages/about.html')
+    mvp_tutors = Tutor.objects.all().filter(is_mvp=True)
+    context = {
+        'mvp_tutors': mvp_tutors,
+    }
+    return render(request, 'pages/about.html', context)
